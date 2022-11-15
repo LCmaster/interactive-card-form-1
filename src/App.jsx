de26ce737d03e0fc5332c816f6372fe4cc10803f
LCmaster <luizcv89@gmail.com>
2022-11-15 00:37:51 -0300

    fixed vertical alignment

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,8 @@ function App() {
   return (
     <div className="font-spaceGrotesk font-medium text-veryDarkViolet ">
       <div className="band -z-10 absolute top-0 left-0 h-[245px] lg:h-screen w-screen lg:w-[483px] bg-mobile lg:bg-desktop bg-cover bg-no-repeat bg-left-top"></div>
-      <div className="mx-auto w-[327px] lg:w-auto px-6 py-8 lg:px-8 flex flex-col justify-center lg:flex-row items-center lg:gap-[124px]">
-        <div className="w-[327px] lg:w-auto lg:flex lg:flex-col-reverse lg:justify-end gap-[37px]">
+      <div className="mx-auto w-[327px] lg:w-auto lg:h-screen px-6 py-8 lg:px-8 flex flex-col lg:flex-row items-center lg:gap-[124px]">
+        <div className="w-[327px] lg:ml-32 lg:w-auto lg:flex lg:flex-col-reverse lg:justify-end gap-[37px]">
           <CardBack cvc={cardInformations.cvc} />
           <CardFront infos={cardInformations} />
         </div>
